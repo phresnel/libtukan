319f6ae846bf70facfc2974bcd4bc18d80552439
Sebastian Mach / phresnel <phresnel@gmail.com>
2013-12-13 12:07:37 +0100

    replace the sunflow color conversion matrix creation with the Lindbloom one

--- include/gaudy/RGBSpace.hh
+++ include/gaudy/RGBSpace.hh
@@ -4,134 +4,82 @@
 #ifndef RGBSPACE_HH_20131122
 #define RGBSPACE_HH_20131122
 
+#include "XYZ.hh"
+#include "Matrix33.hh"
+
 namespace gaudy {
+
+    namespace whitepoint {
+        // TODO: with C++14, the whitepoints should be variable templates.
+        static constexpr XYZ<double> D65 {0.95047, 1.00000, 1.08883};
+    }
 
     template <typename T>
     struct RGBSpace {
-        T rx, ry, rz,
-          gx, gy, gz,
-          bx, by, bz;
+        Matrix33<T> rgb_to_xyz, xyz_to_rgb;
 
-        T xr, yr, zr,
-          xg, yg, zg,
-          xb, yb, zb;
     protected:
-        constexpr RGBSpace(T rx, T ry, T rz,
-                           T gx, T gy, T gz,
-                           T bx, T by, T bz,
-
-                           T xr, T xg, T xb,
-                           T yr, T yg, T yb,
-                           T zr, T zg, T zb) noexcept :
-           rx(rx), ry(ry), rz(rz),
-           gx(gx), gy(gy), gz(gz),
-           bx(bx), by(by), bz(bz),
-
-           xr(xr), yr(yr), zr(zr),
-           xg(xg), yg(yg), zg(zg),
-           xb(xb), yb(yb), zb(zb)
+        constexpr RGBSpace(Matrix33<T> rgb_to_xyz) :
+            rgb_to_xyz(rgb_to_xyz),
+            xyz_to_rgb(inverse(rgb_to_xyz))
         {}
 
         constexpr RGBSpace (RGBSpace const &d) = default;
         constexpr RGBSpace& operator= (RGBSpace const &d) = delete;
 
+
         static constexpr
-        RGBSpace from_xy_quadruple(T xr, T yr, T xg, T yg, T xb, T yb, T xw, T yw) noexcept
+        RGBSpace FromXYTriple(T x_r, T y_r, T x_g, T y_g, T x_b, T y_b, XYZ<double> whitepoint)
+        noexcept
         {
-            return construct(xr,yr, xg,yg, xb,yb, xw,yw,
-                             1-(xr+yr), 1-(xg+yg), 1-(xb+yb), 1-(xw + yw));
+            return lindbloom_method(
+                 {/*X*/ x_r / y_r,              x_g / y_g,              x_b / y_b,
+                  /*Y*/ 1,                      1,                      1,
+                  /*Z*/ (1 - x_r - y_r) / y_r,  (1 - x_g - y_g) / y_g,  (1 - x_b - y_b) / y_b},
+                  whitepoint
+            );
         }
 
     private:
 
-        // The following is a transcription of Sunflow's color space constructor, except it was
-        // partitioned into several functions in order to allow a C++11 constexpr implementation.
-        static constexpr
-        RGBSpace construct (T xr, T yr, T xg, T yg, T xb, T yb, T xw, T yw,
-                            T zr, T zg, T zb, T zw) noexcept
+        static constexpr RGBSpace lindbloom_method (Matrix33<T> M, XYZ<double> whitepoint) noexcept
         {
-            return construct_1(xw, yw, zw,
-                                 // xyz -> rgb matrix, before scaling to white.
-                                 (yg * zb) - (yb * zg),
-                                 (xb * zg) - (xg * zb),
-                                 (xg * yb) - (xb * yg),
-                                 (yb * zr) - (yr * zb),
-                                 (xr * zb) - (xb * zr),
-                                 (xb * yr) - (xr * yb),
-                                 (yr * zg) - (yg * zr),
-                                 (xg * zr) - (xr * zg),
-                                 (xr * yg) - (xg * yr)
-                                 );
+            return lindbloom_method_1(M, inverse(M), whitepoint);
         }
 
-        static constexpr
-        RGBSpace construct_1 (T xw, T yw, T zw,
-                                T rx, T ry, T rz,
-                                T gx, T gy, T gz,
-                                T bx, T by, T bz) noexcept
+        static constexpr RGBSpace lindbloom_method_1(Matrix33<T> M, Matrix33<T> invM,
+                                                     XYZ<double> whitepoint) noexcept
         {
-            return construct_2 (rx, ry, rz,
-                                  gx, gy, gz,
-                                  bx, by, bz,
-                                  // White scaling factors. Dividing by yw scales the white
-                                  // luminance to unity, as conventional.
-                                  ((rx * xw) + (ry * yw) + (rz * zw)) / yw,
-                                  ((gx * xw) + (gy * yw) + (gz * zw)) / yw,
-                                  ((bx * xw) + (by * yw) + (bz * zw)) / yw);
+            return lindbloom_method_2
+                   (
+                      M,
+                      invM._11*whitepoint.X  +  invM._12*whitepoint.Y  +  invM._13*whitepoint.Z,
+                      invM._21*whitepoint.X  +  invM._22*whitepoint.Y  +  invM._23*whitepoint.Z,
+                      invM._31*whitepoint.X  +  invM._32*whitepoint.Y  +  invM._33*whitepoint.Z
+                   );
         }
 
-        static constexpr
-        RGBSpace construct_2(T rx, T ry, T rz,
-                               T gx, T gy, T gz,
-                               T bx, T by, T bz,
-                               T rw, T gw, T bw) noexcept
+        static constexpr RGBSpace lindbloom_method_2(Matrix33<T> M,
+                                                     T S_r, T S_g, T S_b) noexcept
         {
-            // xyz -> rgb matrix, correctly scaled to white
-            return construct_3(rx/rw, ry/rw, rz/rw,
-                                 gx/gw, gy/gw, gz/gw,
-                                 bx/bw, by/bw, bz/bw);
-        }
-
-        static constexpr
-        RGBSpace construct_3(T rx, T ry, T rz,
-                               T gx, T gy, T gz,
-                               T bx, T by, T bz) noexcept
-        {
-            return construct_4(rx, ry, rz,
-                                 gx, gy, gz,
-                                 bx, by, bz,
-                                 // determinant to build inverse
-                                 1 / (rx * (gy*bz - by*gz)
-                                     -ry * (gx*bz - bx*gz)
-                                     +rz * (gx*by - bx*gy)));
-        }
-
-        static constexpr
-        RGBSpace construct_4(T rx, T ry, T rz,
-                               T gx, T gy, T gz,
-                               T bx, T by, T bz,
-                               T s) noexcept
-        {
-            return RGBSpace(rx, ry, rz,
-                            gx, gy, gz,
-                            bx, by, bz,
-                            s*(gy*bz - gz*by), s*(rz*by - ry*bz), s*(ry*gz - rz*gy),
-                            s*(gz*bx - gx*bz), s*(rx*bz - rz*bx), s*(rz*gx - rx*gz),
-                            s*(gx*by - gy*bx), s*(ry*bx - rx*by), s*(rx*gy - ry*gx));
+            return RGBSpace({S_r*M._11, S_g*M._12, S_b*M._13,
+                             S_r*M._21, S_g*M._22, S_b*M._23,
+                             S_r*M._31, S_g*M._32, S_b*M._33});
         }
     };
+
 }
 
 namespace gaudy {
     // sRGB
     template <typename T> struct sRGB : RGBSpace<T> {
      constexpr sRGB() noexcept : RGBSpace<T>(
-      RGBSpace<T>::from_xy_quadruple(0.64,0.33,  0.3,0.6,   0.15,0.06,  0.31271,0.32902)) {} };
+      RGBSpace<T>::FromXYTriple(0.64,0.33,  0.3,0.6,   0.15,0.06,  whitepoint::D65)) {} };
 
     // Adobe RGB
     template <typename T> struct AdobeRGB : RGBSpace<T> {
      constexpr AdobeRGB() noexcept : RGBSpace<T>(
-      RGBSpace<T>::from_xy_quadruple(0.64,0.33,  0.21,0.71, 0.15,0.06,  0.31271,0.32902)) {} };
+      RGBSpace<T>::FromXYTriple(0.64,0.33,  0.21,0.71, 0.15,0.06,  whitepoint::D65)) {} };
 
 }
 
--- src/RGBSpace.cc
+++ src/RGBSpace.cc
@@ -7,147 +7,52 @@
 
 namespace gaudy {
     template <typename T>
-    inline
-    std::ostream& operator<< (std::ostream &os, RGBSpace<T> const &rhs) {
-        return os << "rgb-space{"
-                  << "to-xyz{\n"
-                  << "{" << rhs.xr << ";" << rhs.xg << ";" << rhs.xb << "}\n"
-                  << "{" << rhs.yr << ";" << rhs.yg << ";" << rhs.yb << "}\n"
-                  << "{" << rhs.zr << ";" << rhs.zg << ";" << rhs.zb << "}\n"
-                  << "}\n"
-                  << "to-rgb{\n"
-                  << "{" << rhs.rx << ";" << rhs.ry << ";" << rhs.rz << "}\n"
-                  << "{" << rhs.gx << ";" << rhs.gy << ";" << rhs.gz << "}\n"
-                  << "{" << rhs.bx << ";" << rhs.by << ";" << rhs.bz << "}\n"
-                  << "}"
-                  << "}";
-    }
-
-
-    template <typename T>
     std::ostream& operator<< (std::ostream &os, Matrix33<T> const &m) {
         return os //<< std::fixed
                   << "{(" << m._11 << "," << m._12 << "," << m._13 << "),"
                   << "(" << m._21 << "," << m._22 << "," << m._23 << "),"
                   << "(" << m._31 << "," << m._32 << "," << m._33 << ")}";
     }
+
+    template <typename T>
+    inline
+    std::ostream& operator<< (std::ostream &os, RGBSpace<T> const &rhs) {
+        return os << "rgb-space{"
+                  << "to-xyz{\n"
+                  << rhs.rgb_to_xyz
+                  << "}\n"
+                  << "to-rgb{\n"
+                  << rhs.xyz_to_rgb
+                  << "}"
+                  << "}";
+    }
 }
 
-
-double det(double a00, double a01, double a02,
-          double a10, double a11, double a12,
-          double a20, double a21, double a22)
-{
-    return +a00*(a11*a22-a21*a12)
-           -a01*(a10*a22-a21*a12)
-           +a02*(a10*a21-a11*a20);
-}
-
-void inv(double a00, double a01, double a02,
-         double a10, double a11, double a12,
-         double a20, double a21, double a22,
-         double invdet,
-
-         double &i00, double &i01, double &i02,
-         double &i10, double &i11, double &i12,
-         double &i20, double &i21, double &i22)
-{
-    i00 =  (a11*a22-a21*a12)*invdet;
-    i10 = -(a01*a22-a02*a21)*invdet;
-    i20 =  (a01*a12-a02*a11)*invdet;
-    i01 = -(a10*a22-a12*a20)*invdet;
-    i11 =  (a00*a22-a02*a20)*invdet;
-    i21 = -(a00*a12-a10*a02)*invdet;
-    i02 =  (a10*a21-a20*a11)*invdet;
-    i12 = -(a00*a21-a20*a01)*invdet;
-    i22 =  (a00*a11-a10*a01)*invdet;
-}
-
-void print(std::ostream &os,
-           double a00, double a01, double a02,
-           double a10, double a11, double a12,
-           double a20, double a21, double a22)
-{
-    os << "(" << a00 << " " << a01 << " " << a02 << "\n"
-       << " " << a10 << " " << a11 << " " << a12 << "\n"
-       << " " << a20 << " " << a21 << " " << a22 << ")";
-}
-
-void print_rel(std::ostream &os,
-           double a00, double a01, double a02,
-           double a10, double a11, double a12,
-           double a20, double a21, double a22,
-
-           double b00, double b01, double b02,
-           double b10, double b11, double b12,
-           double b20, double b21, double b22)
-{
-    os << "(" << (a00/b00) << " " << (a01/b01) << " " << (a02/b02) << "\n"
-       << " " << (a10/b10) << " " << (a11/b11) << " " << (a12/b12) << "\n"
-       << " " << (a20/b20) << " " << (a21/b21) << " " << (a22/b22) << ")";
-}
 
 TEST_CASE("gaudy/RGBSpace", "RGBSpace tests") {
+    using namespace gaudy;
 
-    using gaudy::Matrix33;
+    // The following matrices are from Bruce Lindblooms site:
+    REQUIRE(AdobeRGB<float>().rgb_to_xyz == rel_equal(Matrix33<float>
+                                                  {0.5767309,  0.1855540,  0.1881852,
+                                                   0.2973769,  0.6273491,  0.0752741,
+                                                   0.0270343,  0.0706872,  0.9911085},
+                                                   gaudy::epsilon, 0.0000025));
+    REQUIRE(AdobeRGB<float>().xyz_to_rgb == rel_equal(Matrix33<float>
+                                                  {2.0413690, -0.5649464, -0.3446944,
+                                                  -0.9692660,  1.8760108,  0.0415560,
+                                                   0.0134474, -0.1183897,  1.0154096},
+                                                   gaudy::epsilon, 0.0000025));
 
-    /*std::cout << "sRGB\n" << gaudy::sRGB<double>() << "\n";
-              << "AdobeRGB\n" << gaudy::AdobeRGB<double>() << "\n"*/;
 
-    const double x_r = 0.64, y_r=0.33;
-    const double x_g = 0.3,  y_g=0.6;
-    const double x_b = 0.15, y_b=0.06;
-
-    const double X_w = 0.95047, Y_w=1.00000, Z_w=1.08883; // pointed to by Mr Lindbloom upon email: http://www.brucelindbloom.com/index.html?Eqn_ChromAdapt.html
-
-    const Matrix33<double> M_{
-        /*X*/ x_r / y_r,              x_g / y_g,              x_b / y_b,
-        /*Y*/ 1,                      1,                      1,
-        /*Z*/ (1 - x_r - y_r) / y_r,  (1 - x_g - y_g) / y_g,  (1 - x_b - y_b) / y_b
-    };
-
-    const Matrix33<double> invM_ = inverse(M_);
-
-    const double S_r = invM_._11*X_w  +  invM_._12*Y_w  +  invM_._13*Z_w;
-    const double S_g = invM_._21*X_w  +  invM_._22*Y_w  +  invM_._23*Z_w;
-    const double S_b = invM_._31*X_w  +  invM_._32*Y_w  +  invM_._33*Z_w;
-
-    std::cout << "S={" << S_r << "," << S_g << "," << S_b << "}\n";
-    std::cout << "W={" << X_w << "," << Y_w << "," << Z_w << "}\n";
-    std::cout << "----------\n";
-
-    print (std::cout,
-           S_r*M_._11, S_g*M_._12, S_b*M_._13,
-           S_r*M_._21, S_g*M_._22, S_b*M_._23,
-           S_r*M_._31, S_g*M_._32, S_b*M_._33);
-    std::cout << "\n-------------\nshould be:\n";
-    print (std::cout,
-           0.4124564,  0.3575761,  0.1804375,
-           0.2126729,  0.7151522,  0.0721750,
-           0.0193339,  0.1191920,  0.9503041);
-    std::cout << "\n-------------\nrel bruce:\n";
-
-    const Matrix33<double> M {
-        S_r*M_._11, S_g*M_._12, S_b*M_._13,
-        S_r*M_._21, S_g*M_._22, S_b*M_._23,
-        S_r*M_._31, S_g*M_._32, S_b*M_._33,
-    };
-    const Matrix33<double> iM = inverse(M);
-
-    print_rel (std::cout,
-           M._11, M._12, M._13,
-           M._21, M._22, M._23,
-           M._31, M._32, M._33,
-           0.4124564,  0.3575761,  0.1804375,
-           0.2126729,  0.7151522,  0.0721750,
-           0.0193339,  0.1191920,  0.9503041);
-    std::cout << std::endl;
-    print_rel (std::cout,
-           iM._11, iM._12, iM._13,
-           iM._21, iM._22, iM._23,
-           iM._31, iM._32, iM._33,
-           3.2404542, -1.5371385, -0.4985314,
-           -0.9692660,  1.8760108,  0.0415560,
-           0.0556434, -0.2040259,  1.0572252);
-    std::cout << std::endl;
+    REQUIRE(sRGB<float>().rgb_to_xyz == rel_equal(Matrix33<float>
+                                                  {0.4124564, 0.3575761, 0.1804375,
+                                                   0.2126729, 0.7151522, 0.0721750,
+                                                   0.0193339, 0.1191920, 0.9503041},
+                                                   gaudy::epsilon, 0.000001));
+    REQUIRE(sRGB<float>().xyz_to_rgb == rel_equal(Matrix33<float>
+                                                  {3.2404542, -1.5371385, -0.4985314,
+                                                  -0.9692660,  1.8760108,  0.0415560,
+                                                   0.0556434, -0.2040259,  1.0572252},
+                                                   gaudy::epsilon, 0.000001));
 }
